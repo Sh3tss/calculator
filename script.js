e4ccdc76a9fdee5fb17675b18807d499723341bc
Sh3tss <gabrielggartist@gmail.com>
2025-06-11 20:03:40 -0300

    change the html to add the buttons also improved the functions in the js now every operator works as it is

--- script.js
+++ script.js
@@ -1,33 +1,46 @@
-function add(){
-    console.log("you called the add function");
+let first = 0;
+let second = 0;
+let moperator;
+
+function add(a, b){
+    return a + b;
 }
-function subt(){
-    console.log("you called the subtract function");
+function subt(a, b){
+    return a - b;
 }
-function mult(){
-    console.log("you called the multiply function");
+function mult(a, b){
+    return a * b;
 }
-function divi(){
-    console.log("you called the divide function");
+function divi(a, b){
+    if (b === 0){
+        return "Cannot divide by zero!!!";
+    }else{
+        return a / b;
+    }
 }
 
-function operators(){
+
+function operate(first, second, moperator){
+    switch (moperator){
+        case '+':
+            return add(first, second);
+        case '-':
+            return subt(first, second);
+        case '*':
+            return mult(first, second);
+        case '/':
+            return divi(first, second);
+    }
+}
+
+
+function fNumber(){
 
 }
-let input = prompt("choose here");
-let choice = input;
-switch (choice){
-    case add:
-        add();
-        break;
-    case subt:
-        subt();
-        break;
-    case mult:
-        mult();
-        break;
-    case divi:
-        divi();
-        break;
+function ope(){
     
 }
+function sNumber(){
+    
+}
+
